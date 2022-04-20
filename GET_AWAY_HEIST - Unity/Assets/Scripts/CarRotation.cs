8527d76471a6eb91b1084590ba2aef7c782e5543
beo8538 <beo8538@utulsa.edu>
2022-04-20 00:18:55 -0500

    ahhhhhh
    I thought I knew what I was doing.

---
+++ GET_AWAY_HEIST - Unity/Assets/Scripts/CarRotation.cs
@@ -0,0 +1,27 @@
+/***
+ * Created by: Betzaida Ortiz Rivas
+ * Created on: 4/19/2022
+ * 
+ * Edited by:
+ * Edited on:
+ * 
+ * Description: This thing should rotate car when mouse is hovering over it?
+***/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotation; //rotation for the car
+    private float speed; //speed that the rotation is going at
+
+    public void OnPointerEnter() //maybe switch it back to Update()
+    {
+        //Output to see if this works
+        Debug.Log("Cursor on " + name + " GameObject");
+
+        transform.Rotate(rotation * speed * Time.deltaTime); //manages the speed that the object is rotating
+    }
+}
