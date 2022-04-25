daa2e4e630311e5da1e5a3722d89f9a02972d0e1
beo8538 <beo8538@utulsa.edu>
2022-04-25 18:33:21 -0500

    update

--- GET_AWAY_HEIST - Unity/Assets/Scripts/GameManager/GameManager.cs
+++ GET_AWAY_HEIST - Unity/Assets/Scripts/GameManager/GameManager.cs
@@ -8,11 +8,13 @@
  * Description: Basic GameManager Template
 ****/
 
+/*** Using Namespaces ***/
+using System; //C# library for system properties
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
+using UnityEngine.SceneManagement; //libraries for accessing scenes
+using UnityEngine.UI;
 
 //Setting the enum outside the class allows for direct access by the enum (classes) name directly in other classes.
 public enum GameState { Title, Playing, BeatLevel, LostLevel, GameOver, Idle, Testing };
@@ -85,7 +87,7 @@ public class GameManager : MonoBehaviour
 
     [Space(10)]
     public string defaultEndMessage = "Game Over";//the end screen message, depends on winning outcome
-    public string loseMessage = "You Lose"; //Message if player loses
+    public string loseMessage = "You Lose"; //Message if player looses
     public string winMessage = "You Win"; //Message if player wins
     [HideInInspector] public string endMsg;//the end screen message, depends on winning outcome
 
@@ -95,9 +97,6 @@ public class GameManager : MonoBehaviour
 
     [Tooltip("Name of the game over scene")]
     public string gameOverScene;
-
-    [Tooltip("Name of the credits scene")]
-    public string creditsScene;
 
     [Tooltip("Count and name of each Game Level (scene)")]
     public string[] gameLevels; //names of levels
@@ -118,7 +117,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] //Access to private variables in editor
     public bool nextLevel = false; //test for next level
 
-    //Win/Lose conditon
+    //Win/Loose conditon
     [SerializeField] //Access to private variables in editor
     private bool playerWon = false;
 
@@ -242,7 +241,6 @@ public class GameManager : MonoBehaviour
         SetDefaultGameStats(); // the game stats defaults 
 
     }//end StartGame()
-
 
     public void SetDefaultGameStats()
     {
